3964a66c2ee61bca0313de21d2547f3c55d7c488
Vlad Riscutia <vladris@outlook.com>
2018-07-06 20:47:18 -0700

    Add NotNull type.

---
+++ Maki/NotNull.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maki
+{
+    public struct NotNull<T>
+    {
+        public T Item { get; }
+
+        public NotNull(T item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            Item = item;
+        }
+
+        public static implicit operator T(NotNull<T> notNull) => notNull.Item;
+
+        public static implicit operator NotNull<T>(T item) => new NotNull<T>(item);
+    }
+}
---
+++ MakiTest/NotNullTest.cs
@@ -0,0 +1,49 @@
+﻿using Maki;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MakiTest
+{
+    [TestClass]
+    public class NotNullTest
+    {
+        [TestMethod]
+        public void NotNullItemTest()
+        {
+            var notNull = new NotNull<string>("Foo");
+
+            Assert.AreEqual("Foo", notNull.Item);
+        }
+
+        [TestMethod]
+        public void NotNullNullTest()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new NotNull<string>(null));
+        }
+
+        [TestMethod]
+        public void NotNullCastItemTest()
+        {
+            NotNull<string> notNull = "Foo";
+
+            Action<string> action = (s) => Assert.AreEqual("Foo", s);
+
+            action(notNull);
+        }
+
+        [TestMethod]
+        public void NotNullCastNullTest()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => 
+            {
+                NotNull<object> notNull = null;
+            });
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                NotNull<object> notNull = new object();
+                notNull = null;
+            });
+        }
+    }
+}
