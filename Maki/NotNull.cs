6bc55cfc566a9562fae35ffb42194bc7187ae18a
Vlad Riscutia <vladris@outlook.com>
2018-07-06 20:51:19 -0700

    Document NotNull.

--- Maki/NotNull.cs
+++ Maki/NotNull.cs
@@ -4,10 +4,22 @@ using System.Text;
 
 namespace Maki
 {
+    /// <summary>
+    /// Represents an object that cannot be null.
+    /// </summary>
+    /// <typeparam name="T">Type of object.</typeparam>
     public struct NotNull<T>
     {
+        /// <summary>
+        /// Gets the object.
+        /// </summary>
         public T Item { get; }
 
+        /// <summary>
+        /// Creates a new NotNull from the given object.
+        /// </summary>
+        /// <param name="item">Object to store.</param>
+        /// <exception cref="ArgumentNullException">Thrown if the argument is null.</exception>
         public NotNull(T item)
         {
             if (item == null)
@@ -18,8 +30,16 @@ namespace Maki
             Item = item;
         }
 
+        /// <summary>
+        /// Implicit conversion to contained type.
+        /// </summary>
+        /// <param name="notNull">NotNull instance.</param>
         public static implicit operator T(NotNull<T> notNull) => notNull.Item;
 
+        /// <summary>
+        /// Creates a new NotNull from the given object.
+        /// </summary>
+        /// <param name="item">Object to store.</param>
         public static implicit operator NotNull<T>(T item) => new NotNull<T>(item);
     }
 }
